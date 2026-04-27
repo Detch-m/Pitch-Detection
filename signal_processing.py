c69edeb4ef9b0fa1bd6775b4047ef4726ff55a7e
detchm <thanadetch.m@gmail.com>
2026-04-27 07:01:02 -0400

    Doc-Strings finalized. Everything is working.
    Everything works, and Docstrings are added. Unit test still needs to be done on this file. No longer attempts to plot scores over time.

--- signal_processing.py
+++ signal_processing.py
@@ -7,9 +7,15 @@ import librosa
 import matplotlib.pyplot as plt
 
 
-class File_processing:
+class PitchScoring:
     """
-    File processing
+    Represents the pitch scoring system.
+
+    Attributes:
+        ref_file, a String representing the filepath to the vocal version of the song
+        user_file, a String representing the filepath to the user's recording, which should only be their vocal singing
+        ref_pitch, a initialized as none but will hold a numpy float array of reference pitches
+        user_pitch, a initialized as none but will hold a numpy float array of user pitches
     """
 
     def __init__(self, ref_file, user_file):
@@ -17,7 +23,6 @@ class File_processing:
         self.user_file = user_file
         self.ref_pitch = None
         self.user_pitch = None
-        self.alignment_path = None
 
     def extract_pitch(self, audio_file, sensitivity=0.1):
         """
@@ -30,7 +35,7 @@ class File_processing:
 
         Args:
             audio_file, a String representing the name of the audiofile
-            sensitivity, an float representing the sensitivity of the pyin() algorithm
+            sensitivity, an float representing the threshold sensitivity of the pyin() algorithm
                         by default this value is set to 0.1
 
         Returns:
@@ -42,7 +47,7 @@ class File_processing:
             y=audio, sr=sample_rate, fmin=60.0, fmax=1000.0
         )
         pitch_track[~voiced_flag] = np.nan
-        pitch_track[voiced_probs < 0.1] = np.nan
+        pitch_track[voiced_probs < sensitivity] = np.nan
 
         return pitch_track
 
@@ -67,21 +72,21 @@ class File_processing:
         """
         It takes in a frequency and returns the midi score for that frequency.
         A midi score is a semitone unit of measurement with A4 (440 Hz) equalling
-        69 midi score as a standard
+        69 midi score as a standard and following an equation for the rest.
 
         Args:
-            f, a np
+            f, a np array of floats representing the frequency
 
         Returns:
-            midi, representing the midi score
+            a numpy array of floats representing the midi score.
         """
         return 12 * np.log2(f / 440) + 69
 
     def pitch_score(self, level=0):
         """
         A pitch based scoring system where there is two sensitivies for two levels of users.
-        The easy level allows for 1, 2, 5, 12 MIDI-score as error ranges for score deductions. 
-        This is very generous and meant to exist for beginner karaoke users. 
+        The easy level allows for 1, 2, 5, 12 MIDI-score as error ranges for score deductions.
+        This is very generous and meant to exist for beginner karaoke users.
         The hard level allows for 0.25, 0.5, 1, 2 MIDI-score as error ranges, which are meant for
         quarter, half, a full-semitone, or two semitones or errors. This is meant for very confident
         singers in their pitch.
@@ -116,51 +121,18 @@ class File_processing:
             scores.append(score)
         return float(np.mean(scores))
 
-    # ---------------------------
-    # 6. Final score
-    # ---------------------------
     def final_score(self):
-        pitch = self.pitch_score()
-        print("pitch", self.pitch_score())
-        final = pitch
-        return final
+        """
+        Returns the pitch score as the final score.
+        """
+        return self.pitch_score()
 
-    # ---------------------------
-    # 7. Graphing function
-    # ---------------------------
     def plot_results(self):
-        # if self.alignment_path is None:
-        #    raise ValueError("Run align_tracks() first.")
         """
-        ref_aligned = []
-        user_aligned = []
-        frame_scores = []
-
-        # cum_scores = []
-
-        for i, j in self.alignment_path:
-            ref_p = self.ref_pitch[i]
-            user_p = self.user_pitch[j]
-
-            ref_aligned.append(ref_p)
-            user_aligned.append(user_p)
-
-        # Compute per-frame pitch score
-        if not np.isnan(ref_p) and not np.isnan(user_p):
-            error = abs(self.hz_to_cents(ref_p) - self.hz_to_cents(user_p))
-            score = np.exp(-error / 50) * 100
-        else:
-            score = 0
-            frame_scores.append(score)
-
-        ref_aligned = np.array(ref_aligned)
-        user_aligned = np.array(user_aligned)
-        frame_scores = np.array(frame_scores)
-        time = np.arange(len(ref_aligned))
+        Creates a plot of midi-score, time (in seconds) graph of the singer.
         """
-
-        time = np.arange(len(self.user_pitch)) * (265) / (len(self.user_pitch))
-        # ---- Plot 1: Pitch comparison ----
+        duration = librosa.get_duration(path=self.user_file)
+        time = np.arange(len(self.user_pitch)) * (duration) / (len(self.user_pitch))
         plt.figure()
         plt.plot(
             time, np.array(self.hz_to_midi(self.ref_pitch)), label="Reference Pitch"
@@ -173,26 +145,13 @@ class File_processing:
         plt.grid()
         plt.show()
 
-        # ---- Plot 2: Score in frames ----
-        # plt.figure()
-        # plt.plot(time, frame_scores)
-        # plt.title("Score Over Time")
-        # plt.xlabel("Time (frames)")
-        # plt.ylabel("Score (0-100)")
-        # plt.grid()
-        # plt.show()
-        # reference           # user
 
-        # print(len(user_aligned))
-
-
-processor = File_processing(
+processor = PitchScoring(
     "I Can't Help Falling In Love With You - Elvis Presley.mp4", "Recording.wav"
 )
+processor = PitchScoring("PerfectVocals_2.wav", "Itim_Perfect.wav")
 
-processor = File_processing("PerfectVocals_2.wav", "Itim_Perfect.wav")
-
-# processor = File_processing("Perfect_Vocals.wav", "PerfectVocals_2.wav")
+# processor = PitchScoring("Perfect_Vocals.wav", "PerfectVocals_2.wav")
 processor.process_files()
 processor.align_tracks()
 scores = processor.final_score()
